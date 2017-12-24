0973334e0abd91be8015153072b6193da2d066d2
filip <filipjasic8@gmail.com>
2017-12-24 20:30:45 +0100

    v1.1--prvi deo odradjen

--- NoiseGenerator Sim/Debug/sine_table.d
+++ NoiseGenerator Sim/Debug/sine_table.d
@@ -1,9 +1,9 @@
 # FIXED
 
 sine_table.obj: ../sine_table.c
-sine_table.obj: C:/Users/NN/Desktop/DSP/v2/NoiseGenerator\ Sim/c55xx_csl/inc/tistdtypes.h
+sine_table.obj: G:/Downloads/dsp1-projekat/Projekat_0/NoiseGenerator\ Sim/c55xx_csl/inc/tistdtypes.h
 sine_table.obj: ../sine_table.h
 
 ../sine_table.c: 
-C:/Users/NN/Desktop/DSP/v2/NoiseGenerator\ Sim/c55xx_csl/inc/tistdtypes.h: 
+G:/Downloads/dsp1-projekat/Projekat_0/NoiseGenerator\ Sim/c55xx_csl/inc/tistdtypes.h: 
 ../sine_table.h: 
--- NoiseGenerator Sim/gen_sinus.c
+++ NoiseGenerator Sim/gen_sinus.c
@@ -17,28 +17,26 @@ Int16 N = SINE_TABLE_SIZE;
 void gen_sinus_table(Int16 n, Int16 a, float f, float ph, Int16 buffer[])
 {
  
-  /* TODO: DONE */
-	int i = 0;
-	int delta = f * SINE_TABLE_SIZE;
-	int k = (ph/(2*PI)*SINE_TABLE_SIZE);
+		int i = 0;
+		int delta = f * N;
 
+		int k = (ph/(2*PI)* N);
+		int mask = (N-1);
 
-	int mask = (SINE_TABLE_SIZE-1);
-
-	for(i = 0; i < n; i++) {
-		k = k & mask;
-	//prvi kvadrant
-		if(k <= SINE_TABLE_SIZE)
-			buffer[i] = _smpy(a , p_sine_table[k]);
-		if(k > SINE_TABLE_SIZE && k <= 2*SINE_TABLE_SIZE)
-	//drugi kvadrant
-			buffer[i] = _smpy(a , p_sine_table[2*SINE_TABLE_SIZE - k]);
-		if(k > 2*SINE_TABLE_SIZE && k <= 3*SINE_TABLE_SIZE)
-	//treci kvadrant	
-		buffer[i] = _smpy(a , p_sine_table[k - 2*SINE_TABLE_SIZE]);
-		if(k > 3*N && k <= 4*N )
-	//cetvrti kvadrant
-			buffer[i] = _smpy(a , p_sine_table[4*SINE_TABLE_SIZE - k]);
-		k+=delta;
+		for (i = 0; i < n; i++)
+		{
+			k = k & mask;
+			if(k < N) {
+				buffer[i] = a*p_sine_table[k];
+			}
+			else if((k >= N) && (k < N*2)) {
+				buffer[i] = a*p_sine_table[N*2-k];
+			}
+			else if((k >= 2*N) && (k < N*3)) {
+				buffer[i] = -a*p_sine_table[k-2*N];
+			} else
+				buffer[i] = -a*p_sine_table[4*N - k ];
+			k += delta;
+		}
+		ph=k;
 	}
-}
